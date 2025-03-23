85ced4657d318cae980ca8c78bccf073dd5cdcbc
Krithi S J <150175750+kri1105@users.noreply.github.com>
2025-03-23 15:29:16 +0530

    Delete BACK-END/polaris directory

--- BACK-END/polaris/polaris/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import RedirectView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/map/', include('map.urls')),
-    path('', RedirectView.as_view(url='/api/map/', permanent=True)),
-]
